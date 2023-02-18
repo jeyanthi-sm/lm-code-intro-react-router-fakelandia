ac8438730cfd6f991457139b4115bbe3dec763c9
Meenashi <jeyanthi_sm@yahoo.com>
2023-02-18 15:20:57 +0000

    Added Footer and their tes cases

---
+++ src/components/FooterFakelandia.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import FooterFakeLandia from "./FooterFakelandia";
+describe("Testing Basic FooterFakelandia Component", () => {
+  test("Testing Basic FooterFakelandia Component", () => {
+    render (<FooterFakeLandia />);
+    expect(screen.getByText("Fakelandia")).toBeInTheDocument();
+  });
+});
---
+++ src/components/FooterFakelandia.tsx
@@ -0,0 +1,6 @@
+const FooterFakeLandia :React.FC =() => (
+    <>
+    <h4> Fakelandia citizen &copy; </h4>
+    </>
+)
+export default FooterFakeLandia;
