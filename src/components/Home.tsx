f39f93ace51b7a47e8c116235ad4898481568004
Meenashi <jeyanthi_sm@yahoo.com>
2023-02-19 15:36:29 +0000

    Added classname to the MainBody for styling update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,2 +1,7 @@
-const Home:React.FC =() => <> Home! </>
+import MainBody from "./MainBody";
+const Home:React.FC =() => <> Home! 
+<main className="mainClass">
+    <MainBody />
+  </main>
+  </>
 export default Home;
