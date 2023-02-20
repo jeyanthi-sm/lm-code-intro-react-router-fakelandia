7e93ca13add56664e2fff4dee10aeadd8ca36b47
Meenashi <jeyanthi_sm@yahoo.com>
2023-02-20 12:38:06 +0000

    Added Subject input for Confession Screen

--- src/components/Confession.tsx
+++ src/components/Confession.tsx
@@ -1,2 +1,30 @@
-const Confession:React.FC = () => <> Confession! </>
+import { useState } from "react";
+import ConfessionSubject from "./ConfessionSubject";
+const Confession:React.FC = () => {
+type formData = {
+    subject:string;
+    contactReason:string;
+    contactDetails:string;
+}
+const [formInput, setFormInput ] = useState<formData>({
+   subject:"subject",
+   contactReason:"Confession",
+   contactDetails:"Details for contact",
+});
+return (
+<> 
+<div className="mainClass"> 
+<ul>
+    <li> It is very difficult to catch people committing misdemanours so we appreciate it when citizens confess to us directly</li>
+    <li> However if you're just having a hard day and need to vent then you're welcome to contact us here too. Upto you!  </li>
+</ul>
+</div>
+<ConfessionSubject subjectValue="Subject" onChangeHandler={ (e) => {
+    const newFormInput = {...formInput};
+    newFormInput.subject = e;
+    setFormInput(newFormInput);
+}}/>
+</>
+)
+}
 export default Confession;
