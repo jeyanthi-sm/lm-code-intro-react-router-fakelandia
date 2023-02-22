d968fd80040f157c36af2da241e439452c86371c
Meenashi <jeyanthi_sm@yahoo.com>
2023-02-22 22:21:14 +0000

    Added test cases for validateContactReasonDetails input

--- src/components/ContactReasonDetails.tsx
+++ src/components/ContactReasonDetails.tsx
@@ -1,25 +1,22 @@
 import React  from "react";
 import { useState } from "react";
 import { ContactReasonProps } from "./ContactReason";
-export interface contactReasonDetailsProps {
+import ErrorMessage from "./ErrorMessage";
+import { validateContactReasonDetails } from "./utils/validateConfessionContactReasonDetails";
+export interface ContactReasonDetailsProps {
     reasonDetails:string;
     onChangeHandler: (value:string) => void;
 }
 
-const ContactReasonDetails:React.FC<ContactReasonProps> = (inputProps) => {
+const ContactReasonDetails:React.FC<ContactReasonDetailsProps> = (inputProps) => {
 
     const [contactReasonDetails, setContactReasonDetails] = useState();
     const [errorValidation, setErrorValidation] = useState("");
-   const validateContactReasonDetails = (input:string) => {
-    const contactReasonDetailsRegexp = RegExp(/^(.|\n){17,153}$/, "i"); //find any character not between a-z
-    const matchPattern = contactReasonDetailsRegexp.test(input);
-    if (!matchPattern)
-      return "ERROR! Length  Must be between 17 and 153 characters Numbers and Special Characters Not allowed";
-    else return "OK";
-   };
    
     return (
     <>
+    <div className="confessionInputs">
+      
        <label htmlFor="ContactReasonDetailsId"> </label>
        <textarea
          id="ContactReasonDetailsId"
@@ -27,13 +24,18 @@ const ContactReasonDetails:React.FC<ContactReasonProps> = (inputProps) => {
          value={contactReasonDetails}
          onChange={(e) => {
            const errorMessage = validateContactReasonDetails(e.target.value);
-           setErrorValidation(errorMessage);
+           if (typeof errorMessage === "string") setErrorValidation(errorMessage);
            inputProps.onChangeHandler(e.target.value);
          }}
        />
-       {!errorValidation }
-       <div> {errorValidation}</div>
-    </>
+      </div>
+      {errorValidation !== "OK" && (
+        <div>
+          <ErrorMessage message={errorValidation} />
+        </div>
+      )}
+     
+</>
     
     )  
  }
