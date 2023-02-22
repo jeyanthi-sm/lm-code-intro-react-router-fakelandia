a741b116e4f34aee7a9fe5815eec881cd02b6e6e
Meenashi <jeyanthi_sm@yahoo.com>
2023-02-22 20:33:02 +0000

    Added test cases for ContactReason validation

--- src/components/ContactReason.test.tsx
+++ src/components/ContactReason.test.tsx
@@ -1,10 +1,35 @@
-import ContactReason from "./ContactReason";
+import ContactReason, { ContactReasonProps } from "./ContactReason";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 const mockFunction = jest.fn();
 describe("Testing Basic Contact Reason Component", () => {
   test("Testing Basic Contact Reason Component", () => {
     render (<ContactReason reason="lift"
          onChangeHandler={mockFunction} />);
-    expect(screen.getByText("rudeness")).toBeInTheDocument();
+    expect(screen.getByText("Reason for contact")).toBeInTheDocument();
+  });
+});
+
+const mockContactReasonProps: ContactReasonProps = {
+  onChangeHandler: mockFunction,
+  reason: 'lift',
+};
+
+function beforeEachTest() {
+  render (<ContactReason {...mockContactReasonProps} />);
+}
+describe("Change to Contact Reason", () => {
+  test("captures Changes", async () => {
+    beforeEachTest();
+    const CONTACTREASONNEWVALUE = "just-talk";
+    const user = userEvent.setup();
+
+    const node = screen.getByRole("combobox");
+
+    await user.selectOptions(node, CONTACTREASONNEWVALUE);
+
+    expect(mockFunction).toHaveBeenCalledTimes(1);
+    expect(mockFunction).toHaveBeenCalledWith(CONTACTREASONNEWVALUE);
+    
   });
 });
