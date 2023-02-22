5463be28682b53ac616d35dae52019291cf7a736
Meenashi <jeyanthi_sm@yahoo.com>
2023-02-22 13:13:39 +0000

    Mainlayout test cases are updated to test for Home, Misdemeanour and Confession

--- src/components/MainLayout.test.tsx
+++ src/components/MainLayout.test.tsx
@@ -4,13 +4,28 @@ import { BrowserRouter, MemoryRouter } from "react-router-dom";
 
 describe("Testing Basic MainLayout Component using MainLayout Router", () => {
   test("Testing Basic MainLayout Component", () => {
+    const justiceDeparmentElement = screen.getByText(/FAKELANDIA JUSTICE DEPARTMENT/i);
+    const homeElement = screen.queryByRole('link',{name:/Misdemeanour/i});
+    const misdemeanourElement = screen.queryByRole('link',{name:/Misdemeanour/i});
+    const confessionElement = screen.queryByRole('link',{name:/confession/i});
     render (<MemoryRouter><MainLayout /></MemoryRouter>);
-    expect(screen.getByText(/FAKELANDIA JUSTICE DEPARTMENT/i)).toBeInTheDocument();
-  });
+    expect(justiceDeparmentElement).toBeInTheDocument();
+    expect(homeElement).toBeInTheDocument();
+    expect(misdemeanourElement).toBeInTheDocument();
+    expect(confessionElement).toBeInTheDocument();});
 });
-describe("Testing Basic MainLayout Component using Browser Router", () => {
+
+describe("Testing Basic MainLayout Component for Home, Confession and Misdemeanour Links", () => {
   test("Testing Basic MainLayout Component", () => {
+    const homeElement = screen.getByText(/FAKELANDIA JUSTICE DEPARTMENT/i);
+    const misdemeanourElement = screen.queryByRole('link',{name:/Misdemeanour/i});
+    const confessionElement = screen.queryByRole('link',{name:/confession/i});
+ 
     render (<BrowserRouter><MainLayout /></BrowserRouter>);
-    expect(screen.getByText(/FAKELANDIA JUSTICE DEPARTMENT/i)).toBeInTheDocument();
-  });
+  
+    expect(homeElement).toBeInTheDocument();
+    expect(misdemeanourElement).toBeInTheDocument();
+    expect(confessionElement).toBeInTheDocument();
 });
+});
+ 
