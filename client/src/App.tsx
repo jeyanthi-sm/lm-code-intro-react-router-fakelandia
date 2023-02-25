bd212dd830879bbeab4df6bd3123019f3ef6bf99
Meenashi <jeyanthi_sm@yahoo.com>
2023-02-25 14:37:59 +0000

    Initial Client Setup files

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <> 
+    <div className="App">
+    <BrowserRouter> 
+      <FakelandiaRouter />
+    </BrowserRouter>
+    </div>
+    </>
+    );
+}
+
+export default App;
