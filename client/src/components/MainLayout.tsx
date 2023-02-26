dde6af4e15b85bdfc7a724bf9f4d144df638d94f
Meenashi <jeyanthi_sm@yahoo.com>
2023-02-26 17:00:21 +0000

    Navigation Style is added for "isActive"

--- client/src/components/MainLayout.tsx
+++ client/src/components/MainLayout.tsx
@@ -1,21 +1,45 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, NavLink } from "react-router-dom";
 import FooterFakeLandia from "./FooterFakelandia";
+const activeStyle = {
+  textDecoration: "overline", color:"green",
+}
+const nonActiveStyle = {
+   color:"gray",
+}
+
+
 const MainLayout: React.FC = () => (
+ 
+  
   <>
   <header className="headerClass"> 
   <nav >
    <ul>
       <li> FAKELANDIA JUSTICE DEPARTMENT</li>
-      <li> <Link to='/'> Home </Link></li>
-      <li> <Link to='/Misdemeanour' > Misdemeanour</Link></li>   
-      <li> <Link to='/Confession'> Confession to us</Link>        </li>
+      <li> <NavLink to='/'  style={({ isActive }) =>
+              isActive ? activeStyle : nonActiveStyle
+            }> Home </NavLink></li>
+      <li> <NavLink to='/Misdemeanour' style={({ isActive }) =>
+              isActive ? activeStyle : nonActiveStyle
+            }> Misdemeanour</NavLink></li>   
+      <li> <NavLink to='/Confession' style={({ isActive }) =>
+              isActive ? activeStyle : nonActiveStyle
+            }> Confession to us</NavLink>        </li>
     </ul>
   </nav>
-  <Outlet />
+  
+
+  
+
+
+
   </header>
+  <body>
+  <Outlet />
+  </body>
   <footer className="footerClass">
     <FooterFakeLandia />
   </footer>
   </>
-);
+)
 export default MainLayout;
